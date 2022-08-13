a6f29042a871054cd183e3db6361e21a246949e5
rounak adhikary <rounakadhikary1998@gmail.com>
2022-08-13 22:12:22 +0530

    New: Add path header in ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,41 @@ import Issue, {IssueView, IssueCreateView} from './issue/issue.jsx';
 import Epic, {EpicView, EpicCreateUpdateView}  from './epic/epic.jsx';
 import Login, {TicketeerNav} from './Home.jsx';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useLocation, useNavigate, BrowserRouter, Routes, Route } from "react-router-dom";
+
+function PathHeader(){
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathNames = location.pathname.split("/");
+  
+  // home page
+  if(pathNames[1]==="") return <span />;
+
+  let paths = [];
+  let i = 1; let redirectPath = "";
+  for (; i < pathNames.length-1; i++) {
+    redirectPath += "/" + pathNames[i] ;
+    paths.push(
+      <button type="button" onClick={(e)=>navigate(redirectPath)} className="btn btn-secondary btn-sm">
+        {pathNames[i]}
+      </button>);
+    paths.push(<span className="p-1 m-1">></span>);
+  }
+  // the current active subpath
+  paths.push(
+    <button type="button" role="button" aria-disabled="true" className="btn btn-secondary btn-sm disabled">
+      {pathNames[pathNames.length-1]}
+    </button>
+  );
+  return(<div>{paths}</div>);
+}
 
 function AppComponent(props) {
   const elem = (
-    <div className=" m-md-4 m-sm-2 m-xs-1">
+    <div className="m-md-4 m-sm-2 m-xs-1">
       <div className="container-fluid">
+        <PathHeader />
         {props.children}
       </div>
     </div>
@@ -29,6 +58,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={
+          <AppComponent>
+            <div />
+          </AppComponent>
+        } />
         <Route path="login" element={
           <AppComponent>
             <Login />
